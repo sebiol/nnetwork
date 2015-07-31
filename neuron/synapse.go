44a72ec4e74950a44d80159578e6be390a2566be
sebiol <mail@sebiol.de>
2015-07-31 16:58:24 +0200

    Export Weight and Neuron of Synapse
    Removes necessity for getter/setter.
    Values for Neuron and Weight need to be accessible from outside the synapse
    for the learning phase.

--- neuron/synapse.go
+++ neuron/synapse.go
@@ -1,22 +1,10 @@
 package neuron
 
 type Synapse struct {
-  weight    float64
-  neuron   *Neuron
-}
-
-func (s *Synapse) SetNeuron(neuron *Neuron) {
-  s.neuron = neuron
-}
-
-func (s *Synapse) GetWeight() {
-  return s.weight
-}
-
-func (s *Synapse) SetWeight(weight float64) {
-  s.weight = weight
+  Weight    float64
+  Neuron   *Neuron
 }
 
 func (s *Synapse) Value() float64 {
-  return s.neuron.GetOutput() * s.weight
+  return s.Neuron.GetOutput() * s.Weight
 }
