d5d44d4db01633658069ee182ce514e1631123fd
sebiol <mail@sebiol.de>
2015-07-31 07:51:05 +0200

    Neuron struct with function pointers
    Neuron can be composed with funtions from functions file.

--- neuron/neuron.go
+++ neuron/neuron.go
@@ -1,1 +1,19 @@
 package neuron
+
+type Neuron struct {
+  input         InputFunction
+  activation    ActivationFunction
+  output        OutputFunction
+}
+
+func (n *Neuron) SetInputfunction(fp InputFunction) {
+  n.input = fp
+}
+
+func (n *Neuron) SetActivationfunction(fp ActivationFunction) {
+  n.activation = fp
+}
+
+func (n *Neuron) SetOutputfunction(fp OutputFunction) {
+  n.output = fp
+}
