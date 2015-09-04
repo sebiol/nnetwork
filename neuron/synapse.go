258818dd468670711b0875376afa51cf13b381ad
sebiol <mail@sebiol.de>
2015-09-04 11:50:23 +0200

    Synpase Interface + implementation
    The threshold of a neuron can be implemented via an inhibiting synapse. This
    makes it possible to train the threshold via the normal learning algorithm.
    
    The Interface is implemented by ConnectingSynapse and InhibitingSynapse.
    
    ConnectingSynapse will be used to connect Neurons with a weighted Synapse.
    InhibitingSynapse will be used as the inhibiting synapse and does not link
    to a Neuron.

--- neuron/synapse.go
+++ neuron/synapse.go
@@ -1,10 +1,46 @@
 package neuron
 
-type Synapse struct {
-  Weight    float64
-  Neuron   *Neuron
+type Synapse interface {
+  Value()     float64
+  Weight()    float64
+  SetWeight(float64)
 }
 
-func (s *Synapse) Value() float64 {
-  return s.Neuron.GetOutput() * s.Weight
+// ConnectingSynapse connects two neurons
+type ConnectingSynapse struct {
+  weight  float64
+  neuron  *Neuron
+}
+
+// Each neuron (apart from- in most cases - the input layer) gets one inhibiting synapse.
+// The InhibitingSynapse works as a threshold for the activation of the neuron.
+// As a Synapse the InhibitingSynapse (Threshold) will be trained by the normal learning method
+type InhibitingSynapse struct {
+  weight  float64
+}
+
+func (s *ConnectingSynapse) Weight() float64 {
+  return s.weight
+}
+
+func (s *ConnectingSynapse) SetWeight(weight float64) {
+  s.weight = weight
+}
+
+func (s *ConnectingSynapse) Value() float64 {
+  return s.neuron.GetOutput() * s.Weight()
+}
+
+//InhibitingSynapse
+
+func (s *InhibitingSynapse) Weight() float64 {
+  return s.weight
+}
+
+func (s *InhibitingSynapse) SetWeight(weight float64) {
+  s.weight = weight
+}
+
+func (s *InhibitingSynapse) Value() float64 {
+  return 1.0 * s.Weight()
 }
--- neuron/synapse_test.go
+++ neuron/synapse_test.go
@@ -2,7 +2,7 @@ package neuron
 
 import "testing"
 
-func TestSynapseValue(t *testing.T) {
+func TestConnectingSynapseValue(t *testing.T) {
   var neuron1 = new(Neuron)
 
   neuronOutput := 1.0
@@ -14,10 +14,39 @@ func TestSynapseValue(t *testing.T) {
 
   want := neuronOutput * synapseWeight
 
-  synapse := &Synapse{ synapseWeight, neuron1 }
+  synapse := &ConnectingSynapse{ synapseWeight, neuron1 }
 
   got := synapse.Value()
   if got != want {
     t.Errorf("Synapse.Value() == %v, want %v", got, want)
+  }
+}
+
+func TestInhibitingSynapseValue(t *testing.T) {
+  synValue  := 1.0
+  synWeight := 0.5
+  want := synValue * synWeight
+
+  synapse := &InhibitingSynapse{ synWeight }
+  got := synapse.Value()
+  if got != want {
+    t.Errorf("Synapse.Value() == %v, want %v", got, want)
+  }
+}
+
+func TestSetWeight(t *testing.T) {
+  synapse1 := &ConnectingSynapse{ 1.0, nil }
+  synapse2 := &InhibitingSynapse{ 1.0 }
+
+  want := 2.33
+  synapse1.SetWeight(want)
+  synapse2.SetWeight(want)
+
+  if synapse1.Weight() != want {
+    t.Errorf("ConnectingSynapse.Weight() == %v, want %v", synapse1.Weight, want)
+  }
+
+  if synapse2.Weight() != want {
+    t.Errorf("InhibitingSynapse.Weight() == %v, want %v", synapse2.Weight, want)
   }
 }
