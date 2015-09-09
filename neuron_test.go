41f89b3a44eaabec5e29a5024f793d0f4929f216
sebiol <mail@sebiol.de>
2015-09-09 14:11:46 +0200

    Test Neuron link to input array
    The neurons of the input layer shall be linked to an input array so that
    the neurons within the network remain unchanged while the network executes.
    To set a new input for a new execution the input array is changed.

--- neuron_test.go
+++ neuron_test.go
@@ -20,3 +20,23 @@ func TestNeuronGetOutput(t *testing.T) {
     t.Errorf("neuron.GetOutput ignored _oCalculated; want %v, got %v", want, got)
   }
 }
+
+func TestNeuronLinkedToInputArray(t *testing.T) {
+  var neuron1 = new(Neuron)
+
+  testInputs := [][]float64 { {1.0}, {2.0}, {3.0}}
+  var input []float64
+
+  neuron1.SetInputfunction(func([]Synapse) float64 { return input[0] })
+  neuron1.SetActivationfunction(IdentityFunction)
+  neuron1.SetOutputfunction(IdentityFunction)
+
+  for _, c := range testInputs {
+    input = c
+    got := neuron1.GetOutput()
+    neuron1.ResetOutput()
+    if got != c[0] {
+      t.Errorf("Expected %v, got %v", c[0], got)
+    }
+  }
+}
